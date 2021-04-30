8b67e194aac33e2567cf98efe4c671626ccbd9b8
Joseph Acevedo <joseph.acevedo@outlook.com>
2021-04-30 14:24:29 -0700

    mvp

--- ImageTinge.py
+++ ImageTinge.py
@@ -4,6 +4,6 @@ import numpy as np
 
 img = np.full((200, 200, 3), (0, 0, 0), np.uint8)
 red_img = np.full(img.shape, (0,0,255), np.uint8)
-fused_img = cv2.addWeighted(img, 0.4, red_img, 0.6, 0)
+fused_img = cv2.hconcat([img, red_img])
 cv2.imshow("tinged red", fused_img)
 cv2.waitKey(0)
--- Mosaic.py
+++ Mosaic.py
@@ -6,8 +6,8 @@ from time import sleep
 
 import matplotlib.pyplot as plt
 from matplotlib.widgets import TextBox
-from ImageRequest import ImageRequest
-from matplotlib.widgets import TextBox
+
+from ImageRequester import get_images
 
 class Mosaic:
 
@@ -19,8 +19,10 @@ class Mosaic:
     MAX_IMAGES_WIDE = 240
     MAX_IMAGES_TALL = 160
 
+    FREE_IMG = 0
+
     # weights for the tinge, original percent, glass percent
-    IMG_TINGE_WEIGHTS = (0.4, 0.6)
+    IMG_TINGE_WEIGHTS = (0.2, 0.8)
 
     def __init__(self):
         plt.ion()
@@ -30,9 +32,12 @@ class Mosaic:
         self.curr_height = 1
 
         # define max dimensions of the image
-        self.img_imgs = [[np.full((100, 100, 3), (0,0,255), np.uint8)] * self.MAX_IMAGES_WIDE] * self.MAX_IMAGES_TALL
+        self.img_imgs = np.full( (self.MAX_IMAGES_WIDE, self.MAX_IMAGES_TALL, 100, 100, 3), 127, np.uint8)
+        # self.img_imgs = [[np.full((100, 100, 3), (127, 127, 127), np.uint8)] * self.MAX_IMAGES_WIDE] * self.MAX_IMAGES_TALL
+        print(f"img_imgs is {self.img_imgs.shape}")
+        self.free_imgs = np.zeros((self.MAX_IMAGES_TALL, self.MAX_IMAGES_WIDE))
+        self.n_free_imgs = self.curr_width + self.curr_height - 1
 
-        self.image_thread = ImageRequest()
         self.stream = cv2.VideoCapture(0)
 
         if not self.stream.isOpened():
@@ -50,15 +55,30 @@ class Mosaic:
         self.curr_question = self.questions[int(random.random() * len(self.questions))]
         print(self.curr_question)
 
-
     def submit_answer(self, text):
         self.text_box.text = ""
         self.curr_width += 1
         self.curr_height += 1
+        self.n_free_imgs += self.curr_width + self.curr_height - 1
         self.curr_question = self.questions[int(random.random() * len(self.questions))]
-        sleep(0.2)
+
+        img_locs = self.generate_img_locs(int(self.n_free_imgs / 2))
+        get_images(text, self.img_imgs, img_locs)
         
-        
+    def generate_img_locs(self, n_imgs):
+        locs = []
+        row, col = 0, 0
+        for i in range(n_imgs):
+            if self.n_free_imgs <= 0:
+                return locs
+            while (self.free_imgs[row,col] != self.FREE_IMG):
+                col = int(self.curr_width  * random.random())
+                row = int(self.curr_height * random.random())
+            locs.append( (row,col) )
+            self.free_imgs[row][col] += 1
+            self.n_free_imgs -= 1
+        return locs
+
     def camera_loop(self):
         fig1, ax1 = plt.subplots(figsize=(self.FIG_WIDTH_IN, self.FIG_HEIGHT_IN))
 
@@ -76,6 +96,7 @@ class Mosaic:
         axim1 = ax1.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         question = plt.figtext(0.5, 0.15, self.curr_question, ha="center", fontsize=12)
         while rval:
+            # print(f"total: {self.curr_width * self.curr_height}\tfree: {self.n_free_imgs}")
             rval, frame = self.stream.read()
 
             frame = self.process_frame(frame)
@@ -85,11 +106,20 @@ class Mosaic:
             fig1.canvas.flush_events()
         
     def process_frame(self, frame):
-        for x in range(self.curr_width):
-            for y in range(self.curr_height):
-                continue
+        tinged_imgs = [ ]
+        frame = cv2.resize(frame, (self.curr_width, self.curr_height))
+        for row in range(self.curr_width):
+            row_imgs = []
+            for col in range(self.curr_height):
+                tinge_color = frame[row, col]
+                tinged_img = self.tinge_img(self.img_imgs[row, col], tinge_color)
+                row_imgs.append(tinged_img)
+            tinged_imgs.append(row_imgs)
+        
+        concatted_frame = cv2.vconcat([cv2.hconcat(list_h)
+                        for list_h in tinged_imgs])
 
-        return cv2.resize(frame, (self.MAX_IMAGES_WIDE, self.MAX_IMAGES_TALL))
+        return concatted_frame#cv2.resize(concatted_frame, (self.curr_width, self.curr_height))
 
     def tinge_img(self, img, rgb):
         # rgb must be a 3-tuple
